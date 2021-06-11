ed960406716c077e458c3e2af020c1ff432f1121
takumi <takumik9823@gmail.com>
2021-06-12 04:52:02 +0900

    [update]シーダーの呼び出しをコメントアウト

--- html/quick-laravel/database/seeds/DatabaseSeeder.php
+++ html/quick-laravel/database/seeds/DatabaseSeeder.php
@@ -12,12 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        // usersテーブルでseederを実行
-        // $this->call(UsersTableSeeder::class);
         // $this->call(EvaluationsTableSeeder::class);
         // $this->call(CategoriesTableSeeder::class);
         // $this->call(BookshelvesTableSeeder::class);
         // $this->call(BooksTableSeeder::class);
-        $this->call(CommentsTableSeeder::class);
+        // $this->call(CommentsTableSeeder::class);
     }
 }
