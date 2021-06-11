f11e5fe47ea0631cace770fa20c6c763f95b9e8e
takumi <takumik9823@gmail.com>
2021-06-12 04:34:24 +0900

    [add]書籍テーブルのシーダーを追加

--- html/quick-laravel/database/seeds/DatabaseSeeder.php
+++ html/quick-laravel/database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         // $this->call(EvaluationsTableSeeder::class);
         // $this->call(CategoriesTableSeeder::class);
-        $this->call(BookshelvesTableSeeder::class);
+        // $this->call(BookshelvesTableSeeder::class);
+        $this->call(BooksTableSeeder::class);
     }
 }
