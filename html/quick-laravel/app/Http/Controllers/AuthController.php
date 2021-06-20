b375d32ec89583911398c8f1f6a42927204f0df5
takumi <takumik9823@gmail.com>
2021-06-20 15:19:44 +0900

    [update]seeder

--- html/quick-laravel/app/Http/Controllers/AuthController.php
+++ html/quick-laravel/app/Http/Controllers/AuthController.php
@@ -8,6 +8,19 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     //
+    // テストコントローラ
+    public function test() {
+        // DBの中にデータがあるか確認
+        // $this->assertDatabaseHas('users', [
+        //     'email' => '98@example.co.jp',
+        // ]);
+
+        // 暗号化テスト
+        // $encryption = encrypt('root');
+        // $decryption = decrypt('eyJpdiI6Ik5KazJXaDIxMEVDbFprRW9hZm9aRHc9PSIsInZhbHVlIjoiUVFIYWR3S3JhMCtreVhrRlZIR05KZz09IiwibWFjIjoiOGY3NTc0N2E1ZDMxODkyZjUzNjQ4NWU4M2E2YTc0Zjc3YTk5MWNkM2I1ODMxMzBmNThkNDUzZDc0YTBhZGQ0YiJ9');
+        // ddd($decryption);
+    }
+    // ログイン画面の表示
     public function showLogin(){
         return view('login.login_form');
     }
